e15af73f9f33c2a31031b93b02fbb30df26b357c
rhit-zellneae <zellneae@rose-hulman.edu>
2022-10-20 07:40:52 -0400

    Added insert sprocs for Animal, AnimalProduct, ArtisanGood

---
+++ queries/insert/Animal.sql
@@ -0,0 +1,30 @@
+use StardewHoes10
+go
+
+create or alter procedure insert_Animal (
+	@Name varchar(20) = null,
+	@BasePrice int = null,
+	@ID int = null output
+) as
+	if @ID is not null and exists (select * from Animal where ID = @ID) begin
+		raiserror('ERROR in insert_Animal: The animal with ID %d already exists.', 14, 1, @ID)
+		return 1
+	end
+	
+	declare @Status int
+
+	if @ID is null or not exists (select * from Item where ID = @ID) begin
+		execute @Status = insert_Item @Name, 0, @BasePrice, @ID output
+		if @Status != 0 begin return @Status end
+	end
+
+	insert into Animal (ID)
+	values (@ID)
+	set @Status = @@ERROR
+	if @Status != 0 begin
+		raiserror('ERROR in insert_Animal: Failed to insert the animal %s with price %d into the Animal table.', 14, 1, @Name, @BasePrice)
+		return @Status
+	end
+
+	print 'insert_Animal: Successfully inserted the animal ' + @Name + ' with price ' + convert(varchar(40), @BasePrice) + ' into the Animal table.'
+go
